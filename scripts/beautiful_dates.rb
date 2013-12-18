40a440caf5cfa54e4f37287334ab488a512bcf92
perci <Percila>
2013-12-18 12:33:59 +0300

    Gemification complete

--- scripts/beautiful_dates.rb
+++ scripts/beautiful_dates.rb
@@ -1,35 +1,52 @@
-#!/usr/bin/env ruby
+require 'active_support/core_ext/integer/inflections'
 
-class BeautifulDates
-  
-  def initialize
-  @date_time = Time.now
-  @date = @date_time.strftime("%d %b %Y")
-  end
+module BeautifulDates
+	extend self
 
-  def get_day
-    regex = /\d{1,2}/
-    @date[regex]
-  end
+	define_method("beautify") do
+		beautify_date
+	end
 
-  def get_suffix
-    suffix = case day[-1].to_i
-      when 1
-        "st"
-      when 2
-        "nd"
-      when 3
-         "rd"
-      else
-          "th"
-      end
-  end
+	def split_date date
+		date.split(/\/|-|\./)
+	end
 
-  def day_with_suffix
+	def datify_date
+		date = split_date self
+		Time.new(date[0].to_i, date[1].to_i, date[2].to_i)
+	end
 
-    day = self.get_day
-    suffix = self.get_suffix
-   @date.sub(/\d{1,2}/, "#{day}#{suffix}")
-        
-  end
+	def ordinalize_day date_time
+		date_time.day.ordinalize
+	end
+
+	def new_date date, day
+		date.sub(/\d{1,2}/, day)
+	end
+
+	def options_array date_time
+		options = []
+		day = ordinalize_day date_time
+
+		option1 = new_date date_time.strftime("%d %B %Y"), day
+		split_option1 = option1.split
+		option2 = split_option1.insert(0, split_option1.delete_at(1)).insert(1, "the").join(" ")
+		option1 = option1.split.insert(1, "of").join(" ")
+
+		options << option1
+		options << option2
+		options
+	end
+
+	def beautify_date
+		if self.class == Time
+			options_array self
+		else
+			date_time = datify_date
+			options_array date_time
+		end
+	end
 end
+
+String.send(:include, BeautifulDates)
+Time.send(:include, BeautifulDates)
--- tests/tests_beautiful_dates.rb
+++ tests/tests_beautiful_dates.rb
@@ -1,12 +1,25 @@
-require_relative 'beautiful_dates'
-require 'timecop'
-require 'active_support/core_ext/integer/inflections'
+require 'test/unit'
+require_relative '../scripts/beautiful_dates2'
 
-describe "day" do
-  let (:time) {Time.now}
-  
-  it "shoould return date with suffix" do
-    BeautifulDates.new.day_with_suffix.should eq(time.strftime("#{time.day.ordinalize} %b %Y"))
-  end
+class TestBeautifulDates2 < Test::Unit::TestCase
+	def test_slash
+		assert_equal("12/12/12".beautify, ["12th of December 0012", "December the 12th 0012"])
+	end
+
+	def test_dash
+		assert_equal("12-12-12".beautify, ["12th of December 0012", "December the 12th 0012"])
+	end
+
+	def test_dot
+		assert_equal("12.12.12".beautify, ["12th of December 0012", "December the 12th 0012"])
+	end
+
+	def test_combination
+		assert_equal("12-12.12".beautify, ["12th of December 0012", "December the 12th 0012"])
+	end
+
+	def test_time_dot_now
+		assert_equal(Time.now.beautify, ["18th of December 2013", "December the 18th 2013"])
+	end
 
 end
